46b9f3a29d07dfe005a8511a4da353c87319410a
Rob Hadfield <rhadfield@slido.com>
2022-12-15 13:58:08 +0000

    meh

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+
+const Datastore = require('nedb');
+const database = new Datastore('vidcast.db');
+database.loadDatabase();
+
+const app = express();
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true,
+    limits: { 
+        fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
+    },
+}));
+
+// add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+// static files
+app.use(express.static(__dirname + '/public'));
+
+// upload single file
+// https://attacomsian.com/blog/uploading-files-nodejs-express
+app.post('/vidcast-api', async (req, res) => {
+    try {
+        console.log(req.query.video_id);
+        database.insert({video_id:req.query.video_id});
+        //send response
+        res.send({
+            status: 200,
+            message: 'Vidcast ID Saved',
+            id: req.query.video_id
+        });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// view engine and routing setup
+const indexRouter = require('./routes/index');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use('/', indexRouter);
+
+//make uploads directory static
+app.use(express.static('uploads'));
+
+//start app 
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => 
+  console.log(`App is listening on port ${port}.`)
+);
---
+++ routes/file-upload.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/file-upload', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+            
+            //Use the mv() method to place the file in the upload directory (i.e. "uploads")
+            avatar.mv('./uploads/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+module.exports = router;
