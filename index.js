02bd45e64d6256935d55de9d2d130e989dbc5d14
Rob Hadfield <rhadfield@slido.com>
2022-12-16 10:36:38 +0000

    added dependencies

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(express.static('public'))
+// add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
-app.listen(process.env.PORT || 3000);
+app.post('/', (req, res) => {
+    try {
+        if (req.query.video_id) {
+            //send response
+            res.send({
+                status: 200,
+                message: 'Vidcast ID Saved',
+                id: req.query.video_id
+            });
+        } else {
+            res.send({
+                status: 200,
+                message: 'No video_id provided :('
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => 
+  console.log(`App is listening on port ${port}.`)
+);
 
 module.exports = app;
